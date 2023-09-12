95ad80ce4bbae99976c5713c97290c4ae3e2c9f7
pearlKinn <jinju000202@naver.com>
2023-09-12 23:19:25 +0900

    :sparkles: Feat: 동적 라우팅, postImgs 캐러셀 구현, 댓글 input 구현

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,78 +1,109 @@
+import { useState } from 'react';
+import S from '../components/FileUpload/FileUpload.module.css';
 import { useParams } from 'react-router-dom';
 import pb from '@/api/pocketbase';
-import { useRef } from 'react';
-import { useState } from 'react';
+import { getPbImageURL } from '@/utils';
+import FormInput from '@/components/FormInput/formInput';
+import debounce from '@/utils/debounce';
+import { useEffect } from 'react';
 
 function Post() {
-  // const {postId} = useParams()
+  const { postId } = useParams();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fileImages] = useState([]);
+  const [formState, setFormState] = useState('');
+  const [postUser, setPostUser] = useState(null);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
-  const formRef = useRef(null);
-  const photoRef = useRef(null);
-  const handleUpdate = async (e) => {
-    e.preventDefault();
+  const handleNextSlide = () => {
+    setCurrentIndex(
+      (currentIndex + 1) % getPbImageURL(postUser, 'photo').length
+    );
+  };
 
-    const photoValue = photoRef.current.files;
+  const handelPrevSlide = () => {
+    setCurrentIndex(
+      (currentIndex - 1 + getPbImageURL(postUser, 'photo').length) %
+        getPbImageURL(postUser, 'photo').length
+    );
+  };
 
-    const formData = new FormData();
+  const handleInput = debounce((e) => {
+    const { value } = e.target;
+    setFormState(value);
+  }, 400);
 
-    if (photoValue.length > 0) {
-      formData.append('photo', photoValue[0]);
+  useEffect(() => {
+    async function getProduct() {
+      try {
+        const post = await pb.collection('posts').getOne(postId);
+        setPostUser(post);
+        setButtonDisabled(post.photo.length <= 1 || currentIndex === 0);
+      } catch (error) {
+        if (!(error in DOMException)) {
+          console.error();
+        }
+      }
     }
-  };
+    getProduct();
+  }, [postId]);
 
-  const [fileImages, setFileImages] = useState([]);
-
-  const handleUpload = (e) => {
-    const { files } = e.target;
-    const fileImages = Array.from(files).map((file) => ({
-      image: URL.createObjectURL(file),
-      label: file.name,
-    }));
-    setFileImages(fileImages);
-  };
-  return (
-    <div>
-      <form
-        encType="multipart/form-data"
-        ref={formRef}
-        onSubmit={handleUpdate}
-        className="flex flex-col gap-2 items-center"
-      >
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="photo">사진</label>
-          <div className="relative">
-            <input
-              type="file"
-              accept="*.jpg,*.png,*.jpeg,*.webp,*.avif"
-              ref={photoRef}
-              name="photo"
-              id="photo"
-              // multiple
-              onChange={handleUpload}
-              className="absolute z-10 w-full h-full opacity-0 cursor-pointer"
-            />
-            <div className="flex gap-2 overflow-x-auto p-2 w-72 h-72 bg-gray500 mx-auto">
-              {fileImages.map((file) => {
-                return (
-                  <img key={file.label} src={file.image} alt={file.label} />
-                );
-              })}
+  if (postUser) {
+    return (
+      <div className="flex flex-col w-72 mt-5 gap-3  mx-auto">
+        <div className="flex bg-gray500 w-72 h-72">
+          {postUser.photo?.map((_, index) => (
+            <div
+              key={index}
+              className={` ${index === currentIndex ? '' : 'hidden'}`}
+            >
+              <img
+                src={getPbImageURL(postUser, 'photo')[currentIndex]}
+                className={S.uploadImage}
+                alt={`Image ${index + 1}`}
+              />
             </div>
-          </div>
+          ))}
         </div>
-        <div className="flex justify-center gap-2 w-full mt-4 mb-6">
+        <div className={S.carouselBtnWrapper}>
           <button
-            type="submit"
-            className="
-              py-1 px-3.5 border-2 bg-primary hover:border-slate-400 rounded-xl
-            "
+            className={S.carouselBtn}
+            onClick={handelPrevSlide}
+            // disabled={buttonDisabled}
           >
-            업로드
+            Pre
+          </button>
+          <button
+            className={S.carouselBtn}
+            onClick={handleNextSlide}
+            // disabled={buttonDisabled}
+          >
+            Next
           </button>
         </div>
-      </form>
-    </div>
-  );
+        <div className="flex justify-between border border-2 border-gray-900 rounded-[15px] w-72 h-7  p-4 items-center gap-2">
+          <FormInput
+            type="text"
+            id="comment"
+            name="comment"
+            label="댓글창"
+            defaultValue={formState.value}
+            onChange={handleInput}
+          />
+          <button
+            onClick={() => {
+              // console.log(formState);
+            }}
+            type="button"
+            aria-label="댓글 게시"
+            className="bg-transparent border-none w-7 h-5 whitespace-nowrap"
+          >
+            게시
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Post;
