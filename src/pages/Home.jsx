c2d0a40709eff80d90386ab6b56d18b52d318915
pearlKinn <jinju000202@naver.com>
2023-09-13 18:18:32 +0900

    :lipstick: Design: 다크모드 적용, 백그라운드 색상 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Feed from '@/components/Feed/Feed';
 import SearchBar from '@/components/SearchBar/SearchBar';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center dark:bg-black">
       <SearchBar />
       <Feed />
+      <Link to={'/writing'}>글쓰기</Link>
     </div>
   );
 }
