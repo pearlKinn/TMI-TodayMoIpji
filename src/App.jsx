16985b4aaf5c04ff1705f27d3941a475556a8657
pearlKinn <jinju000202@naver.com>
2023-09-16 03:04:17 +0900

    :recycle: Refactor: tanstack 쿼리 적용 피드 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      <Toaster />
+    </QueryClientProvider>
   );
 }
 
