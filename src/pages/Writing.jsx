e757966b5c602934c69a97d1fbd247a29c53ec10
pearlKinn <jinju000202@naver.com>
2023-09-16 23:24:46 +0900

    :lipstick: Design: 화면 너비 조정

--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -1,14 +1,12 @@
 import FileUpload from '@/components/FileUpload/FileUpload';
-import SelectEmoji from '@/components/SelectEmoji/SelectEmoji';
 import { Link } from 'react-router-dom';
 
 function Writing() {
   return (
-    <div className="flex flex-col w-72 gap-2 mx-auto dark:bg-black border border-black">
+    <div className="flex flex-col w-[320px] gap-2 mx-auto dark:bg-black border border-black">
       <Link to={'/'} className="p-1 ">
         <img src="/BackIcon.svg" alt="뒤로가기" className="w-3 h-5 mt-2 mb-2" />
       </Link>
-      {/* <SelectEmoji /> */}
       <FileUpload />
     </div>
   );
