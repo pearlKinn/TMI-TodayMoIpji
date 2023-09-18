6e8630dccc59a1dd3b2311f97f376cd843d98a22
seobinbang7 <newmso5382@gmail.com>
2023-09-18 10:49:28 +0900

    routes 수정

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,23 +1,35 @@
-import { lazy } from "react";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-const Home = lazy(() => import("./pages/Home"));
-const SignIn = lazy(() => import("./pages/SignIn"));
-const SignUp = lazy(() => import("./pages/SignUp"));
-const RootLayout = lazy(() => import("./layout/RootLayout"));
+import { createHashRouter } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import { Mypage } from './pages/Mypage';
+import { Post } from './pages/Post';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="signin" element={<SignIn />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="*" element={<Home />} />
-    </Route>
-  )
+const { VITE_PUBLIC_URL } = import.meta.env;
+
+const router = createHashRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: '/mypage',
+          element: <Mypage />,
+        },
+        {
+          path: ':postId',
+          element: <Post />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: VITE_PUBLIC_URL,
+  }
 );
 
 export default router;
