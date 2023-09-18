f9299b6903fc9143973fbc877dd94dbba6d7bba8
NewBsk <pwo1101@hanmail.net>
2023-09-18 09:32:24 +0900

    :sparkles: Feat: props로 onChange전달 및 props.name 추가 propTypes 추가

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,4 @@
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 import S from './Input.module.css';
 
 function Input(props) {
@@ -7,8 +7,10 @@ function Input(props) {
       <input
         id={props.id}
         type={props.type}
+        name={props.name}
         placeholder={`${props.placeholder}`}
-        className={`${S.input} ${props.width} ${props.height} `}
+        className={`${S.input} ${props.width} ${props.height}`}
+        onChange={props.onChange}
       ></input>
     </div>
   );
@@ -19,7 +21,9 @@ export default Input;
 Input.propTypes = {
   id: string,
   type: string,
+  name: string,
   placeholder: string,
   width: string,
   height: string,
+  onChange: func,
 };
