4fbc6aa8f5536578bd4de3246d532528af8f7ea0
GwonH <kk.filament@gmail.com>
2023-09-18 09:14:50 +0900

    💄 Design: Guest Setting Page 컴포넌트 구현

--- src/pages/GuestSetting.jsx
+++ src/pages/GuestSetting.jsx
@@ -1,19 +1,23 @@
-import UserNickname from '@/components/UserNickname';
-import UserPick from '@/components/UserPic';
-import GuestSettingDarkMode from '@/components/GuestSettingDarkMode';
-function Guestsetting() {
+import UserProfilePicture from '@/components/UserProfilePicture/UserProfilePicture';
+import UserNickname from '@/components/UserNickname/UserNickname';
+import LoginButton from '@/components/LoginButton/LoginButton';
+import DarkModeButton from '@/components/DarkModeButton/DarkModeButton';
+function GuestSetting() {
   return (
-    <div className="md:mx-auto md:w-[768px]  bg-yellow-200 pt-[25px] items-center flex flex-col">
-      <UserPick></UserPick>
-      <div className="my-auto h-[90px] flex">
-        <UserNickname userNickname={'로그인해주세요'}></UserNickname>
+    <>
+      <div className=" md:mx-auto md:w-[768px]  pt-[25px] items-center flex flex-col">
+        <UserProfilePicture></UserProfilePicture>
+        <div className="my-auto h-[90px] flex">
+          <UserNickname userNickname={'로그인해주세요'}></UserNickname>
+        </div>
+        <div className="w-full flex justify-center text-gray-900 bg-[#A3C8C1] border-t-2 border-black">
+          <span>설정</span>
+        </div>
+        <DarkModeButton></DarkModeButton>
+        <LoginButton></LoginButton>
       </div>
-      <div className="w-full flex justify-center text-gray-900 bg-[#A3C8C1] border-t-2 border-black">
-        <span>설정</span>
-      </div>
-      <GuestSettingDarkMode></GuestSettingDarkMode>
-    </div>
+    </>
   );
 }
 
-export default Guestsetting;
+export default GuestSetting;
