b11105af0d7e804db647eb5e15d166eaefe6ae86
GwonH <kk.filament@gmail.com>
2023-09-18 09:13:39 +0900

    💄 Design: User Page UI 구현

---
+++ src/components/UserProfilePicture/UserProfilePicture.jsx
@@ -0,0 +1,11 @@
+function UserProfilePicture() {
+  return (
+    <>
+      <div className="border-2 border-black rounded-full">
+        <img src="/src/assets/defalutProfilePic.png" alt="유저프로필" />
+      </div>
+    </>
+  );
+}
+
+export default UserProfilePicture;
