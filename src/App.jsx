b468559cd4a9e8a7eb7871868d9710f7fef6a445
pearlKinn <jinju000202@naver.com>
2023-09-18 11:28:30 +0900

    :bug: Fix: 이동버튼 누를때 게시물이 올라가는 버그 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Toaster from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 
 const queryClient = new QueryClient();
-
+const notify = () => toast('Here is your toast.');
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <div className="App">
+          <button onClick={notify}>Make me a toast</button>
           <main>
             <Outlet />
           </main>
