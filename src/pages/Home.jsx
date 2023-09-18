60c65a1ab489c4c6f7b989ec9bd8d6afb3fe42ad
pearlKinn <jinju000202@naver.com>
2023-09-18 10:32:59 +0900

    :sparkles: Feat: 검색 필터 기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   const handleSearch = (searchValue) => {
     setFilter(searchValue); // 검색어를 상태로 설정
   };
-
+  console.log('필터', filter);
   return (
     <div className="flex flex-col items-center dark:bg-black w-[320px] mx-auto border border-green-800 md:w-[768px]">
       <SearchBar onSearch={handleSearch} />
