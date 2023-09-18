9251a0546269694269ce5ddc5fcf1e55d37387ac
GwonH <kk.filament@gmail.com>
2023-09-18 09:18:22 +0900

    ♻️ Refactor: 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,11 @@
-import { useState } from 'react';
-import Userpage from './pages/userPage';
-import UserProfileEdit from './pages/UserProfileEdit';
-import { Routes, Route, Link } from 'react-router-dom';
-import Guestsetting from './pages/Guestsetting';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="w-screen">
-      <ul>
-        <li>
-          <Link to="/userpage">UserPage로 가기</Link>
-        </li>
-        <li>
-          <Link to="/guestsetting">Guest setting으로 가기</Link>
-        </li>
-        <li>
-          <Link to="/userprofileeidt">User Profile Edit으로 가기</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route
-          path="/userPage"
-          element={
-            <>
-              <Link to="/">홈으로 가기</Link>
-              <Userpage></Userpage>
-            </>
-          }
-        ></Route>
-        <Route
-          path="/guestsetting"
-          element={
-            <>
-              <Link to="/">홈으로 가기</Link>
-              <Guestsetting></Guestsetting>
-            </>
-          }
-        ></Route>
-
-        <Route
-          path="/userprofileeidt"
-          element={<UserProfileEdit></UserProfileEdit>}
-        ></Route>
-      </Routes>
+    <div className="App">
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,9 @@ import App from './App';
 import Home from './pages/Home';
 import Post from './pages/Post';
 import Writing from './pages/Writing';
+import User from './pages/User';
+import GuestSetting from './pages/GuestSetting';
+import UserProfileEdit from './pages/UserProfileEdit';
 
 const router = createHashRouter([
   {
@@ -12,6 +15,9 @@ const router = createHashRouter([
   },
   { path: ':postId', element: <Post /> },
   { path: 'writing', element: <Writing /> },
+  { path: 'user', element: <User /> },
+  { path: 'guestsetting', element: <GuestSetting /> },
+  { path: 'userProfileEdit', element: <UserProfileEdit /> },
 ]);
 
 export default router;
