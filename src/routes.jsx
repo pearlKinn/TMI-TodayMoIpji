1ccd2749502e5803efc4e68b2d1667838965b385
pearlKinn <jinju000202@naver.com>
2023-09-18 20:30:19 +0900

    :recycle: Refactor: 데이터 확장한 후 불러오기

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,27 +8,31 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Welcome from './pages/Welcome';
 import { Mypage } from './pages/Mypage';
+import UserProfileEdit from './pages/UserProfileEdit';
 
 const router = createHashRouter([
   {
     path: '/',
     element: <App />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: ':postId', element: <Post /> },
+      { path: 'writing', element: <Writing /> },
+      // {
+      //   path: 'writing',
+      //   element: (
+      //     <ProtectRoute>
+      //       <Writing />
+      //     </ProtectRoute>
+      //   ),
+      // },
+      { path: 'signin', element: <SignIn /> },
+      { path: 'signup', element: <SignUp /> },
+      { path: 'welcome', element: <Welcome /> },
+      { path: 'mypage', element: <Mypage /> },
+      { path: 'userprofileedit', element: <UserProfileEdit /> },
+    ],
   },
-  { path: ':postId', element: <Post /> },
-  { path: 'writing', element: <Writing /> },
-  { path: 'signin', element: <SignIn /> },
-  { path: 'signup', element: <SignUp /> },
-  { path: 'welcome', element: <Welcome /> },
-  { path: 'mypage', element: <Mypage /> },
-  // {
-  //   path: 'writing',
-  //   element: (
-  //     <ProtectRoute>
-  //       <Writing />
-  //     </ProtectRoute>
-  //   ),
-  // },
 ]);
 
 export default router;
