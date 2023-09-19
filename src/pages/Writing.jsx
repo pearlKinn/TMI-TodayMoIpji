13d525ba48eba0e11feec7b27f88c0d6484cb4a7
pearlKinn <jinju000202@naver.com>
2023-09-19 15:52:49 +0900

    :art: Style: 공백 제거

--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -6,7 +6,6 @@ function Writing() {
     <div className="flex flex-col w-[320px] gap-2 mx-auto dark:bg-black border border-black">
       <Link to={'/'} className="p-1 ">
         <img src="/BackIcon.svg" alt="뒤로가기" className="w-3 h-5 mt-2 mb-2" />
-
       </Link>
       <FileUpload />
     </div>
