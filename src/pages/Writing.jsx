66ef1548ac0bc40cbe224dac5b14a8712a2706f0
pearlKinn <jinju000202@naver.com>
2023-09-19 18:04:25 +0900

    :bug: Fix: 이미지 경로 설정

--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -6,7 +6,7 @@ function Writing() {
   return (
     <div className="flex flex-col gap-2 w-[320px] h-[585px] mx-auto overflow-y-scroll dark:bg-black box-content">
       <Link to={'/'}>
-        <img src="/BackIcon.svg" alt="뒤로가기" className="w-3 h-5 mt-2" />
+        <img src={BackIcon} alt="뒤로가기" className="w-3 h-5 mt-2" />
       </Link>
       <FileUpload />
     </div>
