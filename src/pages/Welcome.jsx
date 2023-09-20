3898f6d0aebf4eef74da026f52c8a5f3013c0cd8
NewBsk <pwo1101@hanmail.net>
2023-09-20 13:39:34 +0900

    :sparkles: Feat: popup 기능 주석처리 및 네비게이션 기능 추가

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,16 @@
 import Button from '@/components/Button/Button';
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Welcome() {
-  const [popup, setPopup] = useState(false);
-  const handlePopup = () => {
-    setPopup(!popup);
+  // const [popup, setPopup] = useState(false);
+  // const handlePopup = () => {
+  //   setPopup(!popup);
+  // };
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/signin');
   };
 
   return (
@@ -22,43 +28,43 @@ function Welcome() {
           fontsize="text-base"
           fontcolor="text-gray900"
           bgcolor="bg-primary"
-          onClick={handlePopup}
+          onClick={handleLogin}
         />
       </div>
-      {popup && <Popup onClose={handlePopup} />}
+      {/* {popup && <Popup onClose={handlePopup} />} */}
     </>
   );
 }
 
-function Popup() {
-  return (
-    <div>
-      <div className="modal w-[296px] h-[346px] mx-auto">
-        <div className="pt-[7px] pl-[7px]">
-          <button className="type=submit">
-            <img src="/public/close-icon.svg" />
-          </button>
-        </div>
-        <div className="flex items-center justify-center pt-5 pb-[53px]">
-          <img className="mx-auto" src="/public/suggest-icon.svg" />
-        </div>
-        <p className="text-base w-[206px] h-[52px] text-center mx-auto">
-          회원만 사용할 수 있어요 &#59;&#40; 로그인하러 가시겠어요?
-        </p>
-        <div className="flex justify-center pt-[40px]">
-          <Button
-            text="로그인 하기"
-            title="로그인 버튼입니다"
-            width="w-[250px]"
-            height="h-[54px]"
-            fontsize="text-base"
-            fontcolor="text-gray900"
-            bgcolor="bg-primary"
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
+// function Popup() {
+//   return (
+//     <div>
+//       <div className="modal w-[296px] h-[346px] mx-auto">
+//         <div className="pt-[7px] pl-[7px]">
+//           <button className="type=submit">
+//             <img src="/public/close-icon.svg" />
+//           </button>
+//         </div>
+//         <div className="flex items-center justify-center pt-5 pb-[53px]">
+//           <img className="mx-auto" src="/public/suggest-icon.svg" />
+//         </div>
+//         <p className="text-base w-[206px] h-[52px] text-center mx-auto">
+//           회원만 사용할 수 있어요 &#59;&#40; 로그인하러 가시겠어요?
+//         </p>
+//         <div className="flex justify-center pt-[40px]">
+//           <Button
+//             text="로그인 하기"
+//             title="로그인 버튼입니다"
+//             width="w-[250px]"
+//             height="h-[54px]"
+//             fontsize="text-base"
+//             fontcolor="text-gray900"
+//             bgcolor="bg-primary"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Welcome;
