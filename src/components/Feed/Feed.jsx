d211ff3acb5a6c0bf6e63ce63f36149a60015500
pearlKinn <jinju000202@naver.com>
2023-09-24 22:55:40 +0900

    :bug: Fix: 최대 갯수 수정

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -12,7 +12,9 @@ const PB_FEED_ENDPOINT = `${PB}/api/collections/posts/records?expand=user`;
 
 async function fetchProducts({ pageParam = 1 }) {
   // pageParam 매개변수 추가
-  const response = await axios(`${PB_FEED_ENDPOINT}&page=${pageParam}`); // pageParam 사용
+  const response = await axios(
+    `${PB_FEED_ENDPOINT}&page=${pageParam}&perPage=50`
+  ); // pageParam 사용
   return await response.data;
 }
 
@@ -85,3 +87,62 @@ function Feed() {
 }
 
 export default Feed;
+
+// import S from './Feed.module.css';
+// import Spinner from '../Spinner';
+// import FeedItem from '../FeedItem.jsx/FeedItem';
+// import axios from 'axios';
+// import { useQuery } from '@tanstack/react-query';
+// import Filter from '/Filter.svg';
+
+// const PB = import.meta.env.VITE_PB_URL;
+// const PB_FEED_ENDPOINT = `${PB}/api/collections/posts/records?expand=user`;
+
+// async function fetchProducts() {
+//   const response = await axios(PB_FEED_ENDPOINT);
+//   return await response.data;
+// }
+
+// function Feed() {
+//   const {
+//     isLoading,
+//     data: postData,
+//     error,
+//   } = useQuery(['posts'], fetchProducts, {
+//     retry: 2,
+//   });
+
+//   let dataItems = postData?.items;
+//   if (isLoading) {
+//     return <Spinner size={160} title="데이터 가져오는 중이에요." />;
+//   }
+
+//   if (error) {
+//     return (
+//       <div role="alert">
+//         <h2>{error.type}</h2>
+//         <p>{error.message}</p>
+//       </div>
+//     );
+//   }
+
+//   if (postData) {
+//     return (
+//       <div>
+//         <h2 className={S.feedTitle}>Feeds</h2>
+//         <div className={S.headerSection}>
+//           <h3 className={S.newLabel}>NEW</h3>
+//           <button type="button" aria-label="필터">
+//             <img src={Filter} alt="필터" />
+//           </button>
+//         </div>
+//         <div className={S.feedWrapper}>
+//           {dataItems?.toReversed().map((item) => (
+//             <FeedItem key={item.id} item={item} />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Feed;
