cfb6c5f4cb77915ca34ebe2c6077166a9928ac45
Pearl Kim <jinju000202@gmail.com>
2023-10-04 20:49:25 +0900

    :recycle: Refactor: 인피니티 스크롤 적용

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,59 +1,85 @@
-import S from './Feed.module.css';
-import Spinner from '../Spinner';
-import FeedItem from '../FeedItem/FeedItem';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import FeedItem from '../FeedItem/FeedItem';
+import Spinner from '../Spinner';
+import S from './Feed.module.css';
 import Filter from '/Filter.svg';
 
 const PB = import.meta.env.VITE_PB_URL;
-const PB_FEED_ENDPOINT = `${PB}/api/collections/posts/records?expand=user&page=1&perPage=50`;
+const PB_FEED_ENDPOINT = `${PB}/api/collections/posts/records?sort=-created&expand=user`;
 
-async function fetchProducts() {
-  const response = await axios(PB_FEED_ENDPOINT);
-  return await response.data;
+async function fetchProducts({ pageParam = 1 }) {
+  const response = await axios(
+    `${PB_FEED_ENDPOINT}&page=${pageParam}&perPage=12`
+  );
+  return response.data;
 }
 
 function Feed() {
   const {
-    isLoading,
     data: postData,
-    error,
-  } = useQuery(['posts'], fetchProducts, {
-    retry: 2,
-  });
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    isFetching,
+  } = useInfiniteQuery(
+    ['posts'],
+    ({ pageParam }) => fetchProducts({ pageParam }), // queryFn 수정
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const maxPage = lastPage.totalItems / 12;
+        const nextPage = allPages.length + 1;
+        return nextPage <= maxPage ? nextPage : undefined;
+      },
+    }
+  );
 
-  let dataItems = postData?.items;
+  let dataItems = postData ? postData.pages?.flatMap((page) => page.items) : [];
 
-  if (isLoading) {
-    return <Spinner size={160} title="데이터 가져오는 중이에요." />;
-  }
+  const { ref, inView } = useInView({ threshold: 0.3 });
 
-  if (error) {
-    return (
-      <div role="alert">
-        <h2>{error.type}</h2>
-        <p>{error.message}</p>
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  return (
+    <div>
+      <h2 className={S.feedTitle}>Feeds</h2>
+      <div className={S.headerSection}>
+        <h3 className={S.newLabel}>NEW</h3>
+        <button type="button" aria-label="필터">
+          <img src={Filter} alt="필터" />
+        </button>
       </div>
-    );
-  }
-
-  if (postData) {
-    return (
+      <div className={S.feedWrapper}>
+        {dataItems?.map((item) => (
+          <FeedItem key={item.id} item={item} />
+        ))}
+      </div>
       <div>
-        <h2 className={S.feedTitle}>Feeds</h2>
-        <div className={S.headerSection}>
-          <h3 className={S.newLabel}>NEW</h3>
-          <button type="button" aria-label="필터">
-            <img src={Filter} alt="필터" />
-          </button>
-        </div>
-        <div className={S.feedWrapper}>
-          {dataItems?.toReversed().map((item) => (
-            <FeedItem key={item.id} item={item} />
-          ))}
-        </div>
+        <button
+          ref={ref}
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+            ? 'Load Newer'
+            : ''}
+        </button>
       </div>
-    );
-  }
+      <div className="flex justify-center">
+        {isFetching && !isFetchingNextPage ? (
+          <Spinner size={160} title="데이터 가져오는 중이에요." />
+        ) : null}
+      </div>
+    </div>
+  );
 }
+
 export default Feed;
